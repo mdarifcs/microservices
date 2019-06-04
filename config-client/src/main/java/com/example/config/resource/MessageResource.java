4646486a0fb2ed869109c5b1a273825be349a5f0
mohammad arif <mdarif.cs@gmail.com>
2019-06-04 18:41:12 +0530

    Change in config-client properties

--- config-client/src/main/java/com/example/config/resource/MessageResource.java
+++ config-client/src/main/java/com/example/config/resource/MessageResource.java
@@ -1,15 +1,17 @@
 package com.example.config.resource;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RequestMapping("/rest")
 @RestController
 public class MessageResource {
 
-	@Value("${message: default Hello}")
+	@Value("${message: Default Hello}")
 	private String message;
 	
 	@GetMapping("/message")
