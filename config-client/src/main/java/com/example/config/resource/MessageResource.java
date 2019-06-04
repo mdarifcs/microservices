851c0c70d567f845c95071f05297d546c49084dd
mohammad arif <mdarif.cs@gmail.com>
2019-06-04 20:09:34 +0530

    change bootstrap

--- config-client/src/main/java/com/example/config/resource/MessageResource.java
+++ config-client/src/main/java/com/example/config/resource/MessageResource.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageResource {
 
-	@Value("${message}")
+	@Value("${message:default}")
 	private String message;
 	
 	@GetMapping("/message")
